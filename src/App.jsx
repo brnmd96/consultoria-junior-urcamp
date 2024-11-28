3ab2ffac68213b19c55fd3918d60027b4175da4e
Bruno Delabari <brnmd96@gmail.com>
2024-11-28 09:10:52 -0300

    feature: - Adiciona Navbar          - Muda o estilo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { Navbar, HeaderSection, Blogs } from "./components";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div>
+      <Navbar />
+      <HeaderSection />
+      <Blogs />
+    </div>
+  );
 }
---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Navbar from "./Navbar";
+import Blogs from "./Blogs";
+import HeaderSection from "./HeaderSection";
+export {
+    Navbar,
+    HeaderSection,
+    Blogs
+}
